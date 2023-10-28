c49b4270cccb67f3c0842da0a1161a27fb6c1595
Sselenso <122260649+Sselenso@users.noreply.github.com>
2023-10-28 13:44:55 +0300

    13:44_28.10.23

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const tabsMenu = document.querySelector('.tabs-menu');
-  const stickyClass = 'sticky';
-  let isSticky = false;
-  let lastScrollPosition = window.pageYOffset;
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButtons = document.querySelectorAll(".tabs-menu-button");
+  const tabs = document.querySelectorAll(".tabs-content");
+  const menuWrap = document.querySelector(".tabs-menu-wrap");
+  const menu = document.querySelector(".tabs-menu");
 
-  function toggleStickyClass() {
-    const currentScrollPosition = window.pageYOffset;
-
-    if (currentScrollPosition > tabsMenu.offsetTop && currentScrollPosition > lastScrollPosition && !isSticky) {
-      tabsMenu.classList.add(stickyClass);
-      isSticky = true;
-    } else if (currentScrollPosition < lastScrollPosition && isSticky) {
-      tabsMenu.classList.remove(stickyClass);
-      isSticky = false;
-    }
-
-    lastScrollPosition = currentScrollPosition;
+  function setActiveTab(sectionId) {
+    menuButtons.forEach(button => {
+      const href = button.getAttribute("href");
+      if (href === sectionId) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
   }
 
-  window.addEventListener('scroll', toggleStickyClass);
+  function toggleStickyClass() {
+    const currentScrollPosition = window.scrollY;
+    const menuWrapOffsetTop = menuWrap.offsetTop;
+
+    if (currentScrollPosition > menuWrapOffsetTop) {
+      menu.classList.add("sticky");
+    } else {
+      menu.classList.remove("sticky");
+    }
+
+    tabs.forEach(tab => {
+      const tabOffsetTop = tab.offsetTop;
+      const sectionId = `#${tab.getAttribute("id")}`;
+
+      if (currentScrollPosition >= tabOffsetTop && currentScrollPosition < tabOffsetTop + tab.offsetHeight) {
+        setActiveTab(sectionId);
+      }
+    });
+  }
+
+  menuButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      const sectionId = button.getAttribute("href");
+      const targetTab = document.querySelector(sectionId);
+      targetTab.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+
+  window.addEventListener("scroll", toggleStickyClass);
 });
-
-
